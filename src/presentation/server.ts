3e3f95b16610dcb7ce2a1789df437a334df59c19
LostoVayne <moises.barrera.dev@gmail.com>
2025-08-03 00:44:05 -0400

    add cronjob adapter pattern

---
+++ src/presentation/server.ts
@@ -0,0 +1,11 @@
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+  public static start() {
+    console.log("Server is running ");
+    CronService.createJob("*/5 * * * * *", () => {
+      const date = new Date();
+      console.log("5 seconds passed", date);
+    });
+  }
+}
