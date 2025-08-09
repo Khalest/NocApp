61960e31afb9a9c3d9c4e4d3502b3b484faf1c63
LostoVayne <moises.barrera.dev@gmail.com>
2025-08-09 14:34:51 -0400

    feat: implement email logging functionality and update dependencies

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
-import * as env from "env-var";
+import * as envVar from "env-var";
+
+// Compatibility shim: support both CJS and ESM builds of env-var
+const env = (envVar as unknown as { default?: typeof envVar } & typeof envVar).default ?? envVar;
 
 export const envs = {
   PORT: env.get("PORT").required().asPortNumber(),
-  MAILER_EMAIL: env.get("MAILER_EMAIL").required().asEmailString(),
+  MAILER_EMAIL: env.get("MAILER_EMAIL").required().asString(),
   MAILER_SECRET_KEY: env.get("MAILER_SECRET_KEY").required().asString(),
+  MAILER_SERVICE: env.get("MAILER_SERVICE").required().asString(),
 };
